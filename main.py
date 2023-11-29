fba302715d6c0b05ec61024a218d391b3e77a48d
Simões_H <140859035+SimoesH10@users.noreply.github.com>
2023-11-29 18:24:29 +0000

    Hugo_Prova

--- main.py
+++ main.py
@@ -1,37 +1,74 @@
 def q1(cidades):
+    
+    lista_idades_maior_de_100 = ["João Pessoa": 432,"Campina Grande": 325,"Patos": 289, "Sousa": 178,"Cajazeiras": 201,"Cabedelo": 45,"Guarabira": 122,"Areia": 177]
+
+    def lista():
+        for i in range(cidades):
+            if cidades >= 100
+            print(i)
+
     return []
+
 
 def q2(lista1, lista2):
-    return []
-
-def q3(valores):
-    return [],[]
-
-def q4(valores):
-    return []
-
-def main():
-    # Teste as questões que você desenvolveu manualmente:
-    idades = {
-        "João Pessoa": 432,
-        "Campina Grande": 325,
-        "Santa Rita": 68,
-        "Patos": 289,
-        "Bayeux": 54,
-        "Sousa": 178,
-        "Cajazeiras": 201,
-        "Cabedelo": 45,
-        "Guarabira": 122,
-        "Areia": 177,
-    }
-    resultado = q1(idades)
-    print("q1:", resultado)
-
+    (soma_esperada, resultado_esperado):
 
     lista1 = [3, -5, 12, 0, -8, 7]
     lista2 = [-2, 10, -1, 6, -4, 9]
-    resultado = q2(lista1, lista2)
-    print("q2:", resultado)
+    lista3 = 0
+
+    for i in range(len(lista1, lista2)):
+        if i <= 0:
+            print(i)
+            for lista3 in sorted(lista1):
+                print(lista3)
+                for lista3 in sorted(lista2):
+                    print(lista3)
+    
+        break
+
+    for lista1 in sorted(lista1):
+        print()
+    
+        for lista2 in sorted(lista2):
+            print(lista2)
+    
+
+def q3(valores):
+
+
+    impa (x):
+        return x % 3 == 0
+
+    impares = [2, 4, 6, 8, 10, 12, 14, 0]
+    impa = filter(impa, impares)
+    print(list(impa))
+
+
+    par(y):
+        return y % 2 == 0
+
+    pares =  [1, 3, 5, 7, 9, 11, 0]
+    par = filter(par, pares)
+    print(list(par))
+    return [],[]
+
+def q4(valores):
+
+
+    entrada = [30, 20, 10]
+
+    for entrada in reversed(entrada):
+        print(entrada)
+    return []
+
+
+
+
+
+
+
+
 
 
 
